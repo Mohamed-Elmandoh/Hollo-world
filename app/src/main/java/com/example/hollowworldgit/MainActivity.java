2e7e2741437fca9a100c66137acdf71b2bd1340f
Mohamed abdelwadoud <mohamedelmandoh@Mohameds-MacBook-Pro.local>
2022-10-13 19:23:24 +0200

    Intial Commit

--- app/src/main/java/com/example/hollowworldgit/MainActivity.java
+++ app/src/main/java/com/example/hollowworldgit/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 //     git remote add origin https://github.com/Mohamed-Elmandoh/Hollo-world.git
+//      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    token for 30 days
 public class MainActivity extends AppCompatActivity {
 
     @Override
