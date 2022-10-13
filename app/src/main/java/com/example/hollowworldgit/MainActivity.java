c6e1208e3d5d182f9e2fe19656fc136636e33ced
mohamedelmandoh <mohamed.abd.elwadoud96@gmail.com>
2022-10-13 19:52:03 +0200

    test

--- app/src/main/java/com/example/hollowworldgit/MainActivity.java
+++ app/src/main/java/com/example/hollowworldgit/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.hollowworldgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//     git remote add origin https://github.com/Mohamed-Elmandoh/Hollo-world.git
-//      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    token for 30 days
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+        // test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
