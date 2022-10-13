eb3d645bd39c6838df76322499cc71c48fb09e00
Mohamed abdelwadoud <mohamedelmandoh@Mohameds-MacBook-Pro.local>
2022-10-13 19:10:52 +0200

    Intial Commit

---
+++ app/src/main/java/com/example/hollowworldgit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hollowworldgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//     git remote add origin https://github.com/Mohamed-Elmandoh/Hollo-world.git
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
